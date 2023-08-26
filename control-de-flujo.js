5aea53735bbc685799a18e29be8a9c288cbf27c3
aalesann <alejandrosanabria74@gmail.com>
2023-08-25 21:13:26 -0300

    Control de flujo - condicionales

---
+++ control-de-flujo.js
@@ -0,0 +1,88 @@
+// let nombre = "Juan";
+// function saludar() {
+//     console.log("Hola " + nombre)
+// }
+// saludar()
+
+
+// function saludar() {
+//     let nombre = 'Juan'
+
+//     console.log("Hola " + nombre)
+// }
+// saludar()
+// console.log(nombre);
+
+
+// if(true){
+//     var nombre = 'Juan'
+//     console.log("Hola " + nombre)
+
+// }
+// console.log(nombre);
+
+
+
+
+// let a = 1, b = "2";
+// Operadores de comparación
+// a == b si contienen el mismo valor - no evalúa el tipo de dato
+// a === b se evalúa tanto tipo de dato como valor en sí.
+// a != b se evalúa si tienen distinto valor
+// a !== b se evalúa si son distinto valor y tipo
+
+// if( a !== b ){
+//     console.log('Es cierto que son distintos');
+// } else {
+//     console.log('No es cierto que son distintos');
+// }
+
+
+
+// let dia = 43;
+// if(dia === 1){
+//     console.log('Lunes')
+// } else if(dia === 2){
+//     console.log('Martes')
+// } else if(dia === 3){
+//     console.log('Miércoles')
+// } else if(dia === 4){
+//     console.log('Jueves')
+// } else if(dia === 5){
+//     console.log('Viernes')
+// } else if(dia === 6){
+//     console.log('Sabado')
+// } else if(dia === 7){
+//     console.log('Domingo')
+// } else {
+//     console.log('Valor ingresado incorrecto')
+// }
+
+
+// let dia = 3;
+// switch (dia) {
+//     case 1:
+//         console.log('Lunes')
+//         break;
+//     case 2:
+//         console.log('Martes')
+//         break;
+//     case 3:
+//         console.log('Miércoles')
+//         break;
+//     case 4:
+//         console.log('Jueves')
+//         break;
+//     case 5:
+//         console.log('Viernes')
+//         break;
+//     case 6:
+//         console.log('Sabado')
+//         break;
+//     case 7:
+//         console.log('Domingo')
+//         break;
+//     default:
+//         console.log('Valor incorrecto')
+//         break;
+// }
